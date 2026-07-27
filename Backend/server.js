45aa3f2384b2cfa75338210b222cb4584e7fcfcd
Bhoomi Kaushik <kaushikbhumika13@gmail.com>
2026-07-27 14:10:28 +0530

    initial-commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,26 @@
 const dotenv = require("dotenv")
 dotenv.config();
+const path = require("path");
 const app = require("./src/app")
 const connectdb = require("./configurations/db.config")
 
 connectdb();
 
+
+// Serve React build
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+
 app.listen(3000 , (req , res) => {
     console.log("server is running..!!")
 })
+
+
+
+
+
